82d20355b3dd6e5501191934bc8307f9dd261fc1
gekoparty <gekoparty@hotmail.com>
2022-02-01 21:42:01 +0100

    added context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from "react";
+import Auth from "./components/Auth";
+import { AuthContext } from "./context/auth-context";
+import Ingredients from "./components/Ingredients/Ingredients";
 
-import Ingredients from './components/Ingredients/Ingredients';
+const App = (props) => {
+  const authContext = useContext(AuthContext);
 
-const App = props => {
-  return <Ingredients />;
+  let content = <Auth />;
+
+  if (authContext.isAuth) {
+    content = <Ingredients />;
+  }
+
+  return content;
 };
 
 export default App;
